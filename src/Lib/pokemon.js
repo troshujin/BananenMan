324460777f5414b4f7f531a53b1d0a44f7a7c75b
TijmenSimons <61019437+troshujin@users.noreply.github.com>
2025-06-15 01:57:32 +0200

    Update pokemon.js

--- src/Lib/pokemon.js
+++ src/Lib/pokemon.js
@@ -19,10 +19,23 @@ export async function getPokemonData(pokemonName) {
   let pkmnData = pokemon.find(p => p.name == pokemonName);
   if (pkmnData) return pkmnData;
 
-  console.log(`[Pokemon] Fetching pokemon data: ${pokemonName}.`)
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-  if (!response.ok) throw new Error(`Pokémon ${pokemonName} not found`);
-  pkmnData = await response.json();
+  if (pokemonName == "meowstic") {
+    pkmnData = {
+      id: 678,
+      name: "meowstic",
+      types: [],
+      sprites: { front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/678.png" },
+      height: -1,
+      weight: -1,
+      stats: {},
+    }
+    return pkmnData
+  } else {
+    console.log(`[Pokemon] Fetching pokemon data: ${pokemonName}.`)
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    if (!response.ok) throw new Error(`Pokémon ${pokemonName} not found`);
+    pkmnData = await response.json();
+  }
 
   pokemon.push(pkmnData);
   globalState.setState(KEYS.data, pokemon);
